4f27c723b8f0a87c079d0c5b4f8977aa59457c0f
NikitaLejnev <catassasin331@gmail.com>
2022-01-25 10:07:35 +0300

    Added users.js Added jsonwebtoken import to helper.js

--- routes/helper.js
+++ routes/helper.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const verifyToken = require("../middlewares/verify-token");
 
 const { createDb } = require("./helper");
@@ -8,4 +9,11 @@ function createDb() {
   return new sqlite3.Database('./db.sqlite'):
 };
 
-module.exports = { createDb, express, sqlite3, router, verifyToken };
+module.exports = {
+  createDb,
+  express,
+  sqlite3,
+  router,
+  jwt,
+  verifyToken,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const { express, jwt, router } = require("./helper");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username === "admin" && password === "password") {
+    res.json({ token: jwt.sign({ username }, "secret") });
+  }
+  res.status(401);
 });
 
 module.exports = router;
